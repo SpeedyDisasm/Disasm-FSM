db22e29aae4044316434bf299fd89b50789987ad
dimmddr <dmitrius.one@gmail.com>
2013-03-25 02:23:13 +0400

    Первая версия с автоматом Мили
    Начнем нумеровать версии. Это пусть будет 0.01
    На данный момент изменена генерация таблицы состояний - не должно быть
    больше нулевых строк. И содана генерация таблицы сигналов. Сам
    дизассемблер пока что не менялся.

--- build_table.pl
+++ build_table.pl
@@ -9,11 +9,14 @@ open my $in, "<", "instruction_list.txt" or die "$!";
 my $state = 0;
 my $maxstate = 0;
 push my @stateTable, [&getClearState];
+push my @signalTable, [&getClearState];
 my @tailTable;
 my @prefixStateOperand = (1, 2, 3, 4, 6, 7, 10, 11, 13, 16); #состояния КА префиксов, если в префиксах был operand size префикс
 my @prefixStateAddress = (2, 4, 7, 8, 11, 12, 13, 15); #состояния КА префиксов, если в префиксах был address size префикс
 
+my $count = 0;
 while (<$in>) {
+	$count++;
 	chomp;
 	/modRM=(\d+)/;
 	my $modRM = $1;
@@ -21,17 +24,21 @@ while (<$in>) {
 	push @imm, $1, $2 if(m'immediate=(\d+)\/(\d+)');
 	s@modRM.*@@;
 	my @bytes = split " ";
-	foreach my $b (@bytes) {
-		$b = hex $b;
-		if(0 != $stateTable[$state][$b]){
-			$state = $stateTable[$state][$b];
-		} else {
-			$maxstate++;
-			push @stateTable, [&getClearState];
-			$stateTable[$state][$b] = $maxstate;
-			$state = $maxstate;
+	if(1 < @bytes) {
+		for my $b (0..@bytes-2) {
+			$b = hex $bytes[$b]; #я очень скоро не смогу вспомнить что я здесь делаю, я уверен в этом.
+			if(0 != $stateTable[$state][$b]){
+				$state = $stateTable[$state][$b];
+			} else {
+				$maxstate++;
+				push @stateTable, [&getClearState];
+				push @signalTable, [&getClearState];
+				$stateTable[$state][$b] = $maxstate;
+				$state = $maxstate;
+			}
 		}
 	}
+	$signalTable[$state][$bytes[-1]] = $count;
 	 #очень много будет пустых состояний, которые еще и не используются, 
 	 #это необходимо для того, чтобы смещение из таблицы для опкодов подходило и сюда и его не надо было вычислять
 
@@ -66,12 +73,20 @@ sub getClearState {
 
 #open my $in, "<", $fileName or die "$!";
 open my $outputState, ">", "state_table.dat" or die "wtf";
+open my $outputSignal, ">", "signal_table.dat" or die "wtf";
 open my $outputTail, ">", "modRM_and_immediate_table.dat" or die "wtf";
 
 print $outputState "opcodeState";
 #вложенные foreach, мрак и ужас
 	my $max = 0;
 foreach(@stateTable) {
+	foreach(@$_) {
+		if($_ > $max) {$max = $_;}
+		#$_ *= 512;
+		print $outputState " dw $_ \n";
+		}
+}
+foreach(@signalTable) {
 	foreach(@$_) {
 		if($_ > $max) {$max = $_;}
 		#$_ *= 512;
